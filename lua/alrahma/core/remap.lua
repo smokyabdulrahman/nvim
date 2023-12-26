1193a5b4e9ed82d664f0dfcf089f4f6feb3efda3
Abdulrahman Alrahma <a.alrahama@gmail.com>
2023-12-26 22:46:53 +0300

    move to oil using <leader>pv

--- lua/alrahma/core/remap.lua
+++ lua/alrahma/core/remap.lua
@@ -1,5 +1,5 @@
 -- Create leader mapping
 vim.g.mapleader = " "
 -- File tree remapping from ":Ex" to "<leader>pv"
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
 
