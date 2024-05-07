368ba5fcb81f192ec8898e4813339df96ff58d83
Abdulrahman Alrahma <abdulrahman@streampay.sa>
2024-05-07 10:01:05 +0300

    add fugitive and remap

--- lua/alrahma/core/remap.lua
+++ lua/alrahma/core/remap.lua
@@ -3,3 +3,11 @@ vim.g.mapleader = " "
 -- File tree remapping from ":Ex" to "<leader>pv"
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
 
+-- don't copy deleted selection to yank register, instead through it in the void
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to computer clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
