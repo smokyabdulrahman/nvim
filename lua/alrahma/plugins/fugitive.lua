0ac4684bae23eecb73f6e36234c819315c8df7d5
Abdulrahman Alrahma <a.alrahama@gmail.com>
2024-06-13 00:31:48 +0300

    add gitblame toggle

--- lua/alrahma/plugins/fugitive.lua
+++ lua/alrahma/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
 	"tpope/vim-fugitive",
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-	end
+	keys = {
+		{ "<leader>gs", "<cmd>Git<CR>",                                desc = "Open fugitive",    mode = "n" },
+		{ "<leader>gb", "<cmd>Git blame<CR>", desc = "Toggle Git Blame", mode = "n" },
+	}
 }
