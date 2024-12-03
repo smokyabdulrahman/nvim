18215e4309cf346d252eb046b3c67bc67bb2fbb1
Abdulrahman Alrahma <a.alrahama@gmail.com>
2024-12-03 18:01:19 +0000

    add lua lsp

--- lua/alrahma/plugins/lsp-zero.lua
+++ lua/alrahma/plugins/lsp-zero.lua
@@ -2,8 +2,21 @@ return {
 	"VonHeikemen/lsp-zero.nvim",
 	version = "v3.x",
 	dependencies = {
+		{
+			-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+			-- used for completion, annotations and signatures of Neovim apis
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 		-- LSP Support
 		"neovim/nvim-lspconfig",
+		{ "Bilal2453/luvit-meta", lazy = true },
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 
@@ -27,57 +40,77 @@ return {
 
 		lsp.preset("recommended")
 
-		local cmp = require('cmp')
+		local cmp = require("cmp")
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 		local cmp_mappings = lsp.defaults.cmp_mappings({
-			['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-			['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-			['<C-y>'] = cmp.mapping.confirm({ select = true }),
+			["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+			["<C-y>"] = cmp.mapping.confirm({ select = true }),
 			["<C-Space>"] = cmp.mapping.complete(),
 		})
 
-		cmp_mappings['<Tab>'] = nil
-		cmp_mappings['<S-Tab>'] = nil
+		cmp_mappings["<Tab>"] = nil
+		cmp_mappings["<S-Tab>"] = nil
 
 		lsp.on_attach(function(client, bufnr)
 			local opts = { buffer = bufnr, remap = false }
 
-			vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-			vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-			vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-			vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-			vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-			vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-			vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-			vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-			vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+			vim.keymap.set("n", "<leader>gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end, opts)
+			vim.keymap.set("n", "<leader>vws", function()
+				vim.lsp.buf.workspace_symbol()
+			end, opts)
+			vim.keymap.set("n", "<leader>vd", function()
+				vim.diagnostic.open_float()
+			end, opts)
+			vim.keymap.set("n", "[d", function()
+				vim.diagnostic.goto_next()
+			end, opts)
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.goto_prev()
+			end, opts)
+			vim.keymap.set("n", "<leader>vca", function()
+				vim.lsp.buf.code_action()
+			end, opts)
+			vim.keymap.set("n", "<leader>vrr", function()
+				vim.lsp.buf.references()
+			end, opts)
+			vim.keymap.set("n", "<leader>vrn", function()
+				vim.lsp.buf.rename()
+			end, opts)
+			vim.keymap.set("i", "<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end, opts)
 		end)
 
-		require('mason').setup({})
-		require('mason-lspconfig').setup({
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
 			-- Replace the language servers listed here
 			-- with the ones you want to install
-			ensure_installed = { 'pyright', 'ruff', 'html', 'gopls' },
+			ensure_installed = { "pyright", "ruff", "html", "gopls", "lua_ls" },
 			handlers = {
 				lsp.default_setup,
 			},
 			setup_nvim_cmp = {
-				mapping = cmp_mappings
+				mapping = cmp_mappings,
 			},
 			set_preferences = {
 				suggest_lsp_servers = false,
 				sign_icons = {
-					error = 'E',
-					warn = 'W',
-					hint = 'H',
-					info = 'I'
-				}
-			}
+					error = "E",
+					warn = "W",
+					hint = "H",
+					info = "I",
+				},
+			},
 		})
 
 		vim.diagnostic.config({
-			virtual_text = true
+			virtual_text = true,
 		})
 	end,
 }
