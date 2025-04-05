64bcea880343a9aa0197c127300b034cc32188d0
Abdulrahman Alrahma <a.alrahama@gmail.com>
2025-04-05 23:07:21 +0100

    move to lspconfig of neovim (introduced in 0.11)

---
+++ lua/alrahma/plugins/codecomplete.lua
@@ -0,0 +1,41 @@
+return {
+	"hrsh7th/nvim-cmp",
+	version = false,
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"L3MON4D3/LuaSnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local opts = {
+			-- Where to get completion results from
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<Tab>"] = nil,
+				["<S-Tab>"] = nil,
+			}),
+			snippets = {
+				expand = function(args)
+					luasnip.lsp_expand(args)
+				end,
+			},
+		}
+		cmp.setup(opts)
+	end,
+	{ "hrsh7th/cmp-nvim-lsp", lazy = true },
+	{ "hrsh7th/cmp-path", lazy = true },
+	{ "hrsh7th/cmp-buffer", lazy = true },
+}
---
+++ lua/alrahma/plugins/lsp.lua
@@ -0,0 +1,141 @@
+-- if you need help; read this -> https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+return {
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+				-- used for completion, annotations and signatures of Neovim apis
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{ "Bilal2453/luvit-meta", lazy = true },
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			-- Swift
+			lspconfig.sourcekit.setup({
+				capabilities = {
+					workspace = {
+						didChangeWatchedFiles = {
+							dynamicRegistration = true,
+						},
+					},
+				},
+			})
+			-- python
+			lspconfig.pyright.setup({})
+			-- lua
+			require("lspconfig").lua_ls.setup({
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME,
+								-- Depending on the usage, you might want to add additional paths here.
+								-- "${3rd}/luv/library"
+								-- "${3rd}/busted/library",
+							},
+							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+							-- library = vim.api.nvim_get_runtime_file("", true)
+						},
+					})
+				end,
+				settings = {
+					Lua = {},
+				},
+			})
+
+			-- mappings
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP Actions",
+				callback = function(args)
+					local opts = { noremap = true, silent = true }
+					vim.keymap.set("n", "<leader>gd", function()
+						vim.lsp.buf.definition()
+					end, opts)
+					vim.keymap.set("n", "K", function()
+						vim.lsp.buf.hover()
+					end, opts)
+					vim.keymap.set("n", "<leader>vws", function()
+						vim.lsp.buf.workspace_symbol()
+					end, opts)
+					vim.keymap.set("n", "<leader>vd", function()
+						vim.diagnostic.open_float()
+					end, opts)
+					vim.keymap.set("n", "[d", function()
+						vim.diagnostic.goto_next()
+					end, opts)
+					vim.keymap.set("n", "]d", function()
+						vim.diagnostic.goto_prev()
+					end, opts)
+					vim.keymap.set("n", "<leader>vca", function()
+						vim.lsp.buf.code_action()
+					end, opts)
+					vim.keymap.set("n", "<leader>vrr", function()
+						vim.lsp.buf.references()
+					end, opts)
+					vim.keymap.set("n", "<leader>vrn", function()
+						vim.lsp.buf.rename()
+					end, opts)
+					vim.keymap.set("i", "<C-h>", function()
+						vim.lsp.buf.signature_help()
+					end, opts)
+				end,
+			})
+
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				-- Replace the language servers listed here
+				-- with the ones you want to install
+				ensure_installed = {
+					"clangd",
+					"rust_analyzer",
+					"pyright",
+					"ruff",
+					"html",
+					"gopls",
+					"lua_ls",
+				},
+				set_preferences = {
+					suggest_lsp_servers = false,
+					sign_icons = {
+						error = "E",
+						warn = "W",
+						hint = "H",
+						info = "I",
+					},
+				},
+			})
+
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
+}
