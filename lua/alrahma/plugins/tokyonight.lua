d9f88e25710883c7e0fbf32f352e83bc6b3ebfb1
Abdulrahman Alrahma <a.alrahama@gmail.com>
2025-04-10 19:50:58 +0100

    remove deprecated code, and change line numbers' colors to a thing i can see while the sun is up.

--- lua/alrahma/plugins/tokyonight.lua
+++ lua/alrahma/plugins/tokyonight.lua
@@ -1,9 +1,26 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-	vim.cmd("colorscheme tokyonight-night")
-  end
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		-- Lua
+		require("tokyonight").setup({
+			-- use the night style
+			style = "night",
+			-- disable italic for functions
+			styles = {
+				functions = {},
+			},
+			-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+			on_colors = function(colors)
+				colors.hint = colors.orange
+				colors.error = "#ff0000"
+			end,
+		})
+		vim.cmd("colorscheme tokyonight-night")
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#51B3EC", bold = true })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "white", bold = true })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#FB508F", bold = true })
+	end,
 }
