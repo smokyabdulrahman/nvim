b2e2d242ea2411bb53b4117b0829cf2f1c4959ec
Abdulrahman Alrahma <a.alrahama@gmail.com>
2026-01-17 21:09:15 +0000

    add folds to nvim

---
+++ lua/alrahma/core/folds.lua
@@ -0,0 +1,8 @@
+vim.opt.foldcolumn = "0"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
