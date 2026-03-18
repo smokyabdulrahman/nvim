ee0485b7c7dabdd5a4ec4ecf8012559e8b97dd9d
Abdulrahman Alrahma <a.alrahama@gmail.com>
2026-03-18 16:43:49 +0000

    revert pyerfly

--- lua/alrahma/core/lsp.lua
+++ lua/alrahma/core/lsp.lua
@@ -20,7 +20,7 @@ vim.diagnostic.config({
 })
 
 -- enabled servers
-vim.lsp.enable("pyrefly")
+vim.lsp.enable("pyright")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("sourcekit")
 -- vim.lsp.enable("ts")
